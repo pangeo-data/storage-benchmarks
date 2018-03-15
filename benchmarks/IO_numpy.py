1a1b081c0d069178b0fbf0f6ef5c7ee78c956a0c
jreadey <jreadey@hdfgroup.org>
2018-03-15 10:51:17 -0700

    initial hsds tests

--- benchmarks/IO_numpy.py
+++ benchmarks/IO_numpy.py
@@ -2,7 +2,7 @@
     IO Tests on Numpy performance
 
 '''
-from . import target_zarr, target_hdf5
+from . import target_zarr, target_hdf5, target_hsds
 import numpy as np
 
 class IORead_zarr_POSIX_local(target_zarr.SingleZarrPOSIXFile):
@@ -38,6 +38,27 @@ class IORead_h5netcdf_POSIX_local(target_hdf5.SingleHDF5POSIXFile):
 
 
 class IOWrite_h5netcdf_POSIX_local(target_hdf5.SingleHDF5POSIXFile):
+    def setup(self):
+        return
+
+    def time_writetest(self):
+        return
+
+    def time_fancywritecalculation(self):
+        return
+
+class IORead_h5netcdf_HSDS(target_hsds.SingleHDF5HSDSFile):
+    def setup(self):
+        return
+
+    def time_readtest(self):
+        return
+
+    def time_fancycalculation(self):
+        return
+
+
+class IOWrite_h5netcdf_HSDS(target_hsds.SingleHDF5HSDSFile):
     def setup(self):
         return
 
--- benchmarks/IO_raw.py
+++ benchmarks/IO_raw.py
@@ -4,7 +4,7 @@
 
 """
 
-from . import target_zarr, target_hdf5
+from . import target_zarr, target_hdf5, target_hsds
 import os
 import tempfile
 import itertools
@@ -62,6 +62,25 @@ class IORead_h5netcdf_POSIX_local(target_hdf5.SingleHDF5POSIXFile):
 
 
 class IOWrite_h5netcdf_POSIX_local(target_hdf5.SingleHDF5POSIXFile):
+    def setup(self):
+        self.create_objects()
+        self.nz = 1024
+        self.ny = 256
+        self.nx = 512
+        self.shape = (self.nz, self.ny, self.nx)
+        self.dtype = 'f8'
+        self.data = np.random.rand(*self.shape).astype(self.dtype)
+
+    def time_writetest(self):
+        self.h5file.create_dataset(_DATASET_NAME, data=self.data)
+        # are these both necessary
+        self.h5file.flush()
+        self.h5file.close()
+
+    def teardown_files(self):
+        self.rm_objects()
+
+class IOWrite_h5netcdf_HSDS(target_hsds.SingleHDF5HSDSFile):
     def setup(self):
         self.create_objects()
         self.nz = 1024
