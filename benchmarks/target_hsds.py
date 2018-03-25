eb9ec99a1ac538e80cc222788427e4debfb6cc01
jreadey <jreadey@hdfgroup.org>
2018-03-25 13:32:34 -0700

    IO_raw updates

--- benchmarks/target_hsds.py
+++ benchmarks/target_hsds.py
@@ -7,54 +7,60 @@ import itertools
 import numpy as np
 import h5pyd
 import time
+from . import getTestConfigValue
 
 _counter = itertools.count()
 _DATASET_NAME = "default"
- 
-     
+      
 
 class SingleHDF5HSDSFile(object):
     """
     Single HSDS File (domain)  
 
-    Note: Test expects that the following environment variables are defined:
-    * HS_USERNAME - HSDS username that test will run under
-    * HS_PASSWORD - HSDS password for given username
-    * HS_ENDPOINT - HSDS http endpoint
+    Note: Test expects that the following config settings are defined:
+    * hs_username - HSDS username that test will run under
+    * hs_password - HSDS password for given username
+    * hs_endpoint - HSDS http endpoint
 
     In addition the asvtest folder needs to have been created.  
     This can be done with the hstouch tool:
        $hstouch /home/${HS_USERNAME}/asvtest/
 
-    """       
-    def create_objects(self, empty=True):
-        suffix = '.h5'
-        # HS_USERNAME (HSDS user accont name) environment is required
-        home_folder = os.path.join("/home", os.getenv("HS_USERNAME"))  
-        self.temp_dir = os.path.join(home_folder, "asvtest/")   
-        self.path = os.path.join(self.temp_dir,
+    """   
+    def __init__(self):
+        self.username = getTestConfigValue("hs_username")
+        self.password = getTestConfigValue("hs_password")
+        self.endpoint = getTestConfigValue("hs_endpoint")
+        if self.username:
+            home_folder = os.path.join("/home", self.username)  
+            self.temp_dir = os.path.join(home_folder, "asvtest/")   
+            suffix = '.h5'
+            self.path = os.path.join(self.temp_dir,
                                  'temp-%s%s' % (next(_counter), suffix))
-        self.h5file = h5pyd.File(self.path, 'w')
 
-        if not empty:
-            self.nz = 1024
-            self.ny = 256
-            self.nx = 512
-            self.shape = (self.nz, self.ny, self.nx)
-            self.dtype = 'f8'
-            # Create a 1GB dataset
-            data = np.random.rand(*self.shape).astype(self.dtype)
-            # self.h5file.create_dataset(_DATASET_NAME, data=data)  # this fails because it's trying to write too much data at once
-            dset = self.h5file.create_dataset(_DATASET_NAME, (self.nz,self.ny,self.nx), dtype = self.dtype)
+    def create_objects(self, empty=True):   
+        self.h5file = h5pyd.File(self.path, 'w', endpoint=self.endpoint, username=self.username, password=self.password)
              
-            # Writing the entire 1GB in one h5pyd call is not yet supported, so write in slices
+        self.nz = getTestConfigValue("num_slices")
+        self.ny = 256
+        self.nx = 512
+        self.shape = (self.nz, self.ny, self.nx)
+        self.dtype = 'f8'
+        # Create a 1GB dataset
+        data = np.random.rand(*self.shape).astype(self.dtype)
+        dset = self.h5file.create_dataset(_DATASET_NAME, (self.nz,self.ny,self.nx), dtype = self.dtype)
+        self.dset_name = _DATASET_NAME
+        # Writing the entire dataset in one h5pyd call is not yet supported for large datasets, so write in slices
+        if not empty:
             for i in range(self.nz):
                 dset[i, :, :] = data[i, :, :]
 
-            self.h5file.close()
+        self.h5file.close()
 
     def rm_objects(self):
-        folder = h5pyd.Folder(self.temp_dir, mode='a')
+        if not self.username or not self.password or not self.endpoint:
+            return
+        folder = h5pyd.Folder(self.temp_dir, mode='a', endpoint=self.endpoint, username=self.username, password=self.password)
         if len(folder) == 0:
             time.sleep(10)  # allow time for HSDS to sync to S3
         for name in folder:
