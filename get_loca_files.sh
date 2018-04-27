dd9fd93e76c1b412794ca9dfe1fc527906f48b38
jreadey <jreadey@hdfgroup.org>
2018-04-27 10:26:24 -0700

    fixed get_local_files to actually work

--- get_loca_files.sh
+++ get_loca_files.sh
@@ -9,28 +9,26 @@ if [ $# -eq 0 ] || ([ $1 == "-h" ] || [ $1 == "--help" ]); then
    echo "Approx 20GB of diskspace will be needed to store entire collection"
    exit 1
 fi 
+LOCA_URL="http://nasanex.s3.amazonaws.com/LOCA/ACCESS1-0/16th/historical/r1i1p1/tasmax"
+TARGET=$1
 startyear=1950
 endyear=2005
 if [ $# -gt 1 ]; then
    startyear=$2
-   startyear=$(($startyear + 0))
 fi
 if [ $# -gt 2 ]; then
    endyear=$3
-   endyear=$(($endyear + 0))
 fi
 
-echo "start:  ($startyear + 1)"
+echo "start: "$startyear
 echo "end: " $endyear
-for i in {$startyear..$endyear}
-do
-   echo "Year $i "
-done
 
-LOCA_URL="http://nasanex.s3.amazonaws.com/LOCA/ACCESS1-0/16th/historical/r1i1p1/tasmax"
-LOCA_NAME="tasmax_day_ACCESS1-0_historical_r1i1p1_19500101-19501231.LOCA_2016-04-02.16th.nc"
-TARGET=$1
-# wget the file
-#wget --directory-prefix=${TARGET} ${LOCA_URL}/${LOCA_NAME}
+for (( i=$startyear; i<=$endyear; i++ ))  
+do
+   LOCA_NAME="tasmax_day_ACCESS1-0_historical_r1i1p1_"${i}"0101-"${i}"1231.LOCA_2016-04-02.16th.nc"
+   echo "Year $i, name $LOCA_NAME "
+   # wget the file
+   wget --directory-prefix=${TARGET} ${LOCA_URL}/${LOCA_NAME}
+done
 
 
